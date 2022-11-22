15ce98de63c9cf82177bab8cc485160fbacd321a
mahmoud ramadan <mahmoudr03@gmail.com>
2022-11-22 21:30:17 +0200

    finish

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// $(function () {
+//     $('[data-toggle="tooltip"]').tooltip()
+//   })
+
+  $('.unread').toolbar({
+    content: '#toolbar-unread',
+      position: 'bottom',
+      event: 'click',
+    hideOnClick: true
+  });
+  $('.login').toolbar({
+    content: '#login',
+      position: 'bottom'
+  });
+
+  $('.link').toolbar({
+    content: '#link',
+      position: 'bottom'
+  });
+  $('.share').toolbar({
+    content: '#share',
+      position: 'bottom'
+  });
+
+ 
+ 
